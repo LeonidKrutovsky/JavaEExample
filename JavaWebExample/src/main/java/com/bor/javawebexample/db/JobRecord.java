0fdf91d593e1af991a980b7108bec67da9186ad7
Крутовский Леонид Викторович <krutovsky_lv@tomsk.lemz.ru>
2017-10-13 18:12:26 +0700

    Add JobRecord class - entity for db

---
+++ JavaWebExample/src/main/java/com/bor/javawebexample/db/JobRecord.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bor.javawebexample.db;
+
+/**
+ *
+ * @author leon
+ */
+public class JobRecord {      
+
+    private int id;
+    private String lastname;
+    private String firstname;
+    private String job;
+    private String address;
+    
+    JobRecord() {}
+    
+    JobRecord(String fname, String lname, String job, String address) {
+        this.firstname = fname;
+        this.lastname = lname;
+        this.job = job;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
