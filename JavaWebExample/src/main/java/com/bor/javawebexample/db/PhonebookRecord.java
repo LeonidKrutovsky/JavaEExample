eb066e22350dcb4af25ed8f39065d40b5be75343
Крутовский Леонид Викторович <krutovsky_lv@tomsk.lemz.ru>
2017-10-13 21:43:36 +0700

    Add PhonebookRecord - db entity

---
+++ JavaWebExample/src/main/java/com/bor/javawebexample/db/PhonebookRecord.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bor.javawebexample.db;
+
+import com.j256.ormlite.field.DataType;
+import com.j256.ormlite.field.DatabaseField;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author leon
+ */
+public class PhonebookRecord {    
+
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField(width = 20)
+    private String lastname;    
+    
+    @DatabaseField(width = 10)
+    private String firstname;
+    
+    @DatabaseField(width = 13)
+    private String workPhone;
+    
+    @DatabaseField(width = 13)
+    private String mobilePhone;
+
+    @DatabaseField(width = 40)
+    private String email;    
+
+    @DatabaseField(dataType = DataType.DATE)
+    private Date birthdate;    
+    
+
+    @DatabaseField
+    private String job;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getWorkPhone() {
+        return workPhone;
+    }
+
+    public void setWorkPhone(String workPhone) {
+        this.workPhone = workPhone;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStringBirthdate() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String retVal = format.format(birthdate);
+        return retVal == null ? "NULL" : retVal;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    
+}
