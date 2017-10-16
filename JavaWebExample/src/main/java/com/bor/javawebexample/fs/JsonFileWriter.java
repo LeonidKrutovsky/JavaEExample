a152a1b87f9e95a9172171aff87881c2534b0d71
Крутовский Леонид Викторович <krutovsky_lv@tomsk.lemz.ru>
2017-10-16 19:01:59 +0700

    write and move json file and file-flag

--- JavaWebExample/src/main/java/com/bor/javawebexample/fs/JsonFileWriter.java
+++ JavaWebExample/src/main/java/com/bor/javawebexample/fs/JsonFileWriter.java
@@ -11,12 +11,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 
 /**
  *
@@ -24,11 +30,12 @@ import java.util.logging.Logger;
  */
 public class JsonFileWriter {
 
-    private File jsondataFile;
-    private ObjectMapper mapper = new ObjectMapper();
+    private final File jsondataFile;
+    private final ObjectMapper mapper = new ObjectMapper();
     private static File workDir;
     private static File prepareDir;
-    private static Logger logger = Logger.getLogger(JsonFileWriter.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(JsonFileWriter.class.getName());
+    private static final DateFormat df = new SimpleDateFormat("ddMMyy_HHmm");
 
     public static void prepareDirs(String mainDirPath) {
         workDir = new File(mainDirPath + File.separator + "work");
@@ -49,21 +56,59 @@ public class JsonFileWriter {
     public void saveRecord(PhonebookRecord record) throws JsonProcessingException {
 //        mapper.enable(SerializationFeature.INDENT_OUTPUT);
         String json = mapper.writeValueAsString(record);
-        
+
         try (FileWriter writer = new FileWriter(jsondataFile, true)) {
             writer.write(json);
         } catch (IOException ex) {
-            logger.log(Level.SEVERE, null, ex);
+            LOGGER.log(Level.SEVERE, null, ex);
         }
 
-        DateFormat df = new SimpleDateFormat("ddMMyy_HHmm");
         Date today = Calendar.getInstance().getTime();
-        File file = new File(prepareDir.getAbsolutePath() + File.separator + "data_" + df.format(today) + ".json" );
+        File file = new File(prepareDir.getAbsolutePath() + File.separator + "data_" + df.format(today) + ".json");
 
         try (FileWriter writer = new FileWriter(file, true)) {
             writer.write(json);
         } catch (IOException ex) {
-            logger.log(Level.SEVERE, null, ex);
+            LOGGER.log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void sendToWork() {
+        File[] files = prepareDir.listFiles();
+        List<String> stringList = new ArrayList<>();
+
+        for (File file : files) {
+            try {
+                List<String> lines = Files.readAllLines(Paths.get(file.getAbsolutePath()), StandardCharsets.UTF_8);
+                stringList.addAll(lines);
+            } catch (IOException ex) {
+                LOGGER.log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        if (stringList.isEmpty()) return;
+
+        Date today = Calendar.getInstance().getTime();
+        String destFilePath = workDir.getAbsolutePath() + File.separator + "json_" + df.format(today);
+        File destinationFile = new File(destFilePath + ".txt");
+
+        if (destinationFile.exists()) {
+            destinationFile.delete();
+        }
+
+        try (FileWriter writer = new FileWriter(destinationFile, true)) {
+            for (String line : stringList) {
+                writer.write(line);
+            }
+            
+            File fileFlag = new File(destFilePath + ".ready");
+            fileFlag.createNewFile();
+        } catch (IOException ex) {
+            LOGGER.log(Level.SEVERE, null, ex);
+        } 
+
+        for (File file : files) {
+            file.delete();
         }
     }
 }
