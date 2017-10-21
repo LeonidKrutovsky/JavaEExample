f35c19594e59e7dad8c759ae1251c395d9b717c6
Крутовский Леонид Викторович <krutovsky_lv@tomsk.lemz.ru>
2017-10-21 14:57:32 +0700

    Set default log file

--- JavaWebExample/src/main/java/com/bor/javawebexample/InitializerContextListener.java
+++ JavaWebExample/src/main/java/com/bor/javawebexample/InitializerContextListener.java
@@ -5,11 +5,17 @@
  */
 package com.bor.javawebexample;
 
-import com.bor.javawebexample.db.JobRecord;
-import com.bor.javawebexample.db.ORMLiteUtils;
-import com.bor.javawebexample.db.PhonebookRecord;
+
 import com.bor.javawebexample.fs.Configuration;
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import org.yaml.snakeyaml.Yaml;
@@ -27,15 +33,21 @@ public class InitializerContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent event) {
 
         if (conf == null) {
-            InputStream input = getClass().getClassLoader().getResourceAsStream("/config.yml");
+            InputStream input = getClass().getClassLoader().getResourceAsStream("/config.yml");            
             if (input == null) {
                 conf = new Configuration(); //default
             } else {
                 conf = new Yaml().loadAs(input, Configuration.class);
             }
         }        
-        event.getServletContext().setAttribute(ATTRIBUTE_NAME, conf);
-    }
+        event.getServletContext().setAttribute(ATTRIBUTE_NAME, conf);        
+        
+        try {
+            System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(conf.getLogFilePath(), true)), true));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(InitializerContextListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
+   }
 
     @Override
     public void contextDestroyed(ServletContextEvent event) {
