b29c0147d344ca988ada990bb9da4d852a8cd5af
Крутовский Леонид Викторович <krutovsky_lv@tomsk.lemz.ru>
2017-10-21 18:59:26 +0700

    Add camel route

---
+++ JavaWebExample/src/main/java/com/bor/javawebexample/route/RouteManager.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bor.javawebexample.route;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ *
+ * @author leon
+ */
+public class RouteManager {
+
+    public RouteManager() {
+        
+        String schemaUrl = getClass().getClassLoader().getResource("/PhonebookRecordSchema.json").getPath();
+        PhonebookRecordValidator.loadSchema(schemaUrl);
+        try {
+            start();
+        } catch (Exception ex) {
+            Logger.getLogger(RouteManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void start() throws Exception {
+        
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() {
+                from("file:/home/leon/JEExample/work?doneFileName=${file:name.noext}.ready&delete=true")
+                        //.split(body().tokenize(System.getProperty("line.separator"))) 
+                        //.filter().method(new JsonFilter(""), "isValid")
+                        .process(new PhonebookRecordValidator())
+                        .to("file:/home/leon/JEExample/data?noop=true");
+            }
+        });
+        context.start();
+    }
+
+}
