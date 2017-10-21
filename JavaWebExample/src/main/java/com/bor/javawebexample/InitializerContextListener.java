606a0323a9fb0b1fbb7d04d647f53fcf6f968e1b
Крутовский Леонид Викторович <krutovsky_lv@tomsk.lemz.ru>
2017-10-21 18:57:12 +0700

    Comment logger configuration

--- JavaWebExample/src/main/java/com/bor/javawebexample/InitializerContextListener.java
+++ JavaWebExample/src/main/java/com/bor/javawebexample/InitializerContextListener.java
@@ -5,17 +5,21 @@
  */
 package com.bor.javawebexample;
 
-
 import com.bor.javawebexample.fs.Configuration;
+import com.bor.javawebexample.route.PhonebookRecordValidator;
 import java.io.BufferedOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.Date;
 import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.StreamHandler;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import org.yaml.snakeyaml.Yaml;
@@ -33,21 +37,33 @@ public class InitializerContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent event) {
 
         if (conf == null) {
-            InputStream input = getClass().getClassLoader().getResourceAsStream("/config.yml");            
+            InputStream input = getClass().getClassLoader().getResourceAsStream("/config.yml");
             if (input == null) {
                 conf = new Configuration(); //default
             } else {
                 conf = new Yaml().loadAs(input, Configuration.class);
             }
-        }        
-        event.getServletContext().setAttribute(ATTRIBUTE_NAME, conf);        
-        
-        try {
-            System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(conf.getLogFilePath(), true)), true));
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(InitializerContextListener.class.getName()).log(Level.SEVERE, null, ex);
+
+//            try {
+//                System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(conf.getLogFilePath(), true)), true));
+//            } catch (FileNotFoundException ex) {
+//                Logger.getLogger(InitializerContextListener.class.getName()).log(Level.SEVERE, null, ex);
+//            }        
+//            Logger logger = Logger.getLogger(InitializerContextListener.class.getName());
+//            try {
+//                FileHandler handler = new FileHandler(conf.getLogFilePath(), true);
+//                handler.setFormatter(new SimpleFormatter());
+//                logger.addHandler(handler);
+//            } catch (IOException | SecurityException ex) {
+//                Logger.getLogger(InitializerContextListener.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//            
+//            logger.log(Level.SEVERE, null, "Heelo2222");
+            
         }
-   }
+
+        event.getServletContext().setAttribute(ATTRIBUTE_NAME, conf);
+    }
 
     @Override
     public void contextDestroyed(ServletContextEvent event) {
